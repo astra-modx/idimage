6ba612238722deeb1f304b382cc9dc4b7858e7c7
Андрей <info@bustep.ru>
2025-02-23 22:27:02 +0700

    version

---
+++ Extras/idimage/core/components/idimage/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'astraw/idimage',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e66204eba3d994ea8b459671ef75499c2b94cc3b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'astraw/idimage' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e66204eba3d994ea8b459671ef75499c2b94cc3b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
